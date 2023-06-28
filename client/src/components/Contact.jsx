c59bd13c04a5191d122f695d979d753801dcb2dc
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2023-06-28 20:34:41 +0530

    Initial Commit

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -20,7 +20,10 @@ const Contact = () => {
       message: message,
     };
     // console.log(dataSend);
-    const BASE_URL = "http://localhost:10000"; // Update the URL or endpoint
+    const BASE_URL =
+      process.env.NODE_ENV === "production"
+        ? "https://portfolio-q546.onrender.com/"
+        : "http://localhost:10000"; // Update the URL or endpoint
 
     const res = await fetch(BASE_URL + "/send", {
       method: "POST",
